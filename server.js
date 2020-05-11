7eceb18b39992ee5dabd9a46f44f8f1133ba84a3
Sai Krishna Kurapati Venkata <saikrishna@Sais-MacBook-Pro.local>
2020-05-11 19:06:43 +0530

    Running express for static pages and chages to avoid common pitfalls

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
-
+const path = require('path');
 const app = express();
 
 const port = "3000";
+console.log("hello there");
+app.use(express.static(path.join(__dirname, './provided/static')));
+
 
 app.get('/', (request, response)=>
     {
-        response.send("Hi there");
+        response.sendFile(path.join(__dirname, './provided/static/index.html'));
+    }
+);
+
+app.get('/speakers', (request, response)=>
+    {
+        response.sendFile(path.join(__dirname, './provided/static/speakers.html'));
     }
 );
 
